f634e854fce8a7b39eb899959a660a13def48cf6
rajkumar <raj345051.com>
2019-06-27 23:54:08 +0530

    updated ml_project file

--- ML_project.py
+++ ML_project.py
@@ -11,20 +11,102 @@ from sklearn.model_selection import GridSearchCV, train_test_split, cross_valida
 from sklearn.metrics import confusion_matrix, accuracy_score
 import os
 os.chdir(os.path.dirname(__file__))
+
 # Read Excel file #####
 df = pd.read_excel('Participants_Data_Final\Data_Train.xlsx')
 
+# data summarization ###
+def summary(df):
+	df.describe()
+	df.describe(include=['object'])
+	df.info()
+	df.head(5)
+	return None
+
 # data cleaning #####
+def datacleaning(df, special_character1=False, digit=False,
+                 nalpha=False):
+	df = df.drop_duplicates(keep='first')
+	df = df.reset_index(drop=True)
+# for title case
+	df = df.apply(lambda x: x.str.strip().str.title() if(x.dtypes == object)
+				  else x)
+    if special_character1:
+        df = df.apply(lambda x: x.str.split(r'[^a-zA-Z.\d\s]').
+                      str[0] if(x.dtypes == object) else x)
+    if digit:
+        df = df.apply(lambda x: x.str.replace(r'\d+', '')
+                      if(x.dtypes == object) else x)
+    if nalpha:
+        df = df.apply(lambda x: x.str.replace(r'\W+', ' ')
+                      if(x.dtypes == object) else x)
+	df = df.apply(lambda x: x.str.strip().str.strip() if(x.dtypes == object)
+				  else x)
+	df['Route'] = df['Route'].str.replace('New Delhi-Cochin', 'Delhi-Cochin')	
+	return df
 
 # missing values treatment ###
+def treat_missingValue(df):
+	df = df.apply(lambda x: x.fillna(x.median(), inplace=True)
+				  if((x.dtypes != object) & (x.isnull().sum() > 0)) else x)
+	return df
 
 # outlier detection & treatment ###
+def detect_outliers(col):
+    outlier1 = []
+    threshold = 3
+    mean = np.mean(col)
+    std =np.std(col)
+    for i in col:
+        z_score = (i - mean)/std 
+        if np.abs(z_score) > threshold:
+            outlier1.append(i)
+    outlier2 = []
+    sorted(col)
+    q1, q3 = np.percentile(col,[25,75])
+    iqr = q3 - q1
+    lower_bound = q1 -(1.5 * iqr) 
+    upper_bound = q3 +(1.5 * iqr)
+    for i in col:
+        if ((i > upper_bound) | (i < lower_bound)):
+            outlier2.append(i)
+    lst1 = np.unique(outlier1)
+    lst2 = np.unique(outlier2)
+    output = list(set(lst1) & set(lst2))
+    return output 
+
+# treat outliers ###
+def treat_outliers(df):
+#	x = treat_outliers(df.Duration)
+#	y = df.Duration.replace(x, df.Duration.median())
+	df = df.apply(lambda x: x.replace(treat_outliers(x), x.median())
+				  if((x.dtypes != object) & (len(treat_outliers(x)) > 0)) else x)
+	return df
 
 # feature engineering ###
+def featureEngineering(df):
+	df['time'] = pd.to_datetime(df['time'])
+	df['months'] = df['time'].dt.month_name()
+	df['day'] = df['time'].dt.day_name()
+	df = df.sort_values(by='time')
+	lb_make = LabelEncoder()
+	y_test = lb_make.fit_transform(df.Holiday)
+	return df
 
-# EDA ###
+# Exploratory Data Analysis ###
+f, axes = plt.subplots(2, 2)
+sns.boxplot(x=df["Dep_Time"], ax=axes[0, 0])
+sns.boxplot(x=df["Arrival_Time"], ax=axes[0,1])
+sns.boxplot(x=df["Duration"] , ax=axes[1,0])
+sns.boxplot(x=df["Price"], ax=axes[1,1])
+sns.pairplot(tab,vars=['Price','Duration','Arrival_Time','Dep_Time'], kind='scatter')
+f, axes = plt.subplots(1, 2)
+sns.countplot(y=df["Airline"], ax=axes[0])
+sns.countplot(y=df["Route"], ax=axes[1])
 
-# feature selection ###
+# Feature Selection ###
+def Feature_selection(df):
+	return df
 
 ## Split the training and tesing datasets from main datasets
 X,y = df.iloc[:,1:11], df.iloc[:,11]
